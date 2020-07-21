8b197ea495527c4e92cc0b800b6c7558cfbf7860
Iman Radjavi <radjavi@hotmail.com>
2020-07-21 10:11:56 +0200

    small changes

--- public/playback.js
+++ public/playback.js
@@ -14,47 +14,51 @@ const initSpotifyPlayer = token => {
     player.on('playback_error', ({ message }) => { console.error(message); });
   
     // Playback status updates
-    player.addListener('player_state_changed', state => { 
+    player.on('player_state_changed', state => { 
       console.log(state); 
-      if (!state) { 
-        current_track_uri = "";
-        $("#track_name").fadeOut(200);
-        $("#track_artist").fadeOut(200);
-        $("#track_img").fadeOut(200);
-        return; 
-      }
-
-      if (state.track_window.current_track.uri != current_track_uri) {
-        current_track_uri = state.track_window.current_track.uri
-
-        $("#track_name").fadeOut(200, () => {
-          $("#track_name").text(state.track_window.current_track.name)
-          $("#track_name").fadeIn(200);
-        })
-  
-        $("#track_artist").fadeOut(200, () => {
-          $("#track_artist").text(state.track_window.current_track.artists[0].name)
-          $("#track_artist").fadeIn(200);
-        })
-        
-        $("#track_img").fadeOut(200, () => {
-          $("#track_img").attr('src', state.track_window.current_track.album.images[0].url)
-          $("#track_img").fadeIn(1000);
-        })
-      }
+      updateUI(state);
     });
   
     // Ready
-    player.addListener('ready', ({ device_id }) => {
+    player.on('ready', ({ device_id }) => {
       console.log('Ready with Device ID', device_id);
     });
   
     // Not Ready
-    player.addListener('not_ready', ({ device_id }) => {
+    player.on('not_ready', ({ device_id }) => {
       console.log('Device ID has gone offline', device_id);
     });
   
     // Connect to the player!
     player.connect();
   };
+}
+
+const updateUI = state => {
+  if (!state) { 
+    current_track_uri = "";
+    $("#track_name").fadeOut(200);
+    $("#track_artist").fadeOut(200);
+    $("#track_img").fadeOut(200);
+    return; 
+  }
+
+  if (state.track_window.current_track.uri != current_track_uri) {
+    current_track_uri = state.track_window.current_track.uri
+
+    $("#track_name").fadeOut(200, () => {
+      $("#track_name").text(state.track_window.current_track.name)
+      $("#track_name").fadeIn(200);
+    })
+
+    $("#track_artist").fadeOut(200, () => {
+      $("#track_artist").text(state.track_window.current_track.artists[0].name)
+      $("#track_artist").fadeIn(200);
+    })
+    
+    $("#track_img").fadeOut(200, () => {
+      $("#track_img").attr('src', state.track_window.current_track.album.images[0].url)
+      $("#track_img").fadeIn(1000);
+    })
+  }
 }
